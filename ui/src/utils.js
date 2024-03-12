54728fdcd3dff1e4eaa32b896e1a72cb0039437d
ajeet goswmi <ajeet.2015.gos@gmail.com>
2024-03-12 19:58:09 +0530

    change the lambda url

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,3 +1,3 @@
 export const API_URL =
-  "https://erkbtq5gthsbt6a45tdz3bspye0trceh.lambda-url.us-east-1.on.aws/task";
+  "https://mydhryeau7kmmasjuashi2mfaq0ldlyx.lambda-url.us-east-1.on.aws/task";
 //export const API_URL = "https://localhost:5000/task";
